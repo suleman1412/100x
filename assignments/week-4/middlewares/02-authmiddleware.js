40759f22c37034c03586407f7786bf15af1ae73d
Suleman <sulemankarigar1412@gmail.com>
2024-10-09 23:22:25 +0530

    auth middleware

--- assignments/week-4/middlewares/02-authmiddleware.js
+++ assignments/week-4/middlewares/02-authmiddleware.js
@@ -9,6 +9,19 @@ const VALID_API_KEY = '100xdevs_cohort3_super_secret_valid_api_key'; // key is 1
 // Middleware to check for a valid API key
 function authenticateAPIKey(req, res, next) {
     //  authenticate APIKey here
+    if(!req.headers['100xdevs-api-key']){
+        res.status(401).json({
+            message: "Invalid or missing API key"
+        })
+    }
+
+    if(req.headers['100xdevs-api-key'] === VALID_API_KEY){
+        return next()
+    }
+    
+    res.status(401).json({
+        message: "Invalid or missing API key"
+    })
 }
 
 app.use(authenticateAPIKey);
@@ -16,6 +29,10 @@ app.use(authenticateAPIKey);
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Access granted' });
 });
+
+app.listen(3000, () => {
+    console.log("Server is running on 3000")
+})
 
 module.exports = app;
 
