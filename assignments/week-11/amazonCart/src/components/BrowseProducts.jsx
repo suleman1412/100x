2383ad83d1a2d6cec5ddbfff8d4a6ec1b0896c5d
Suleman <sulemankarigar1412@gmail.com>
2024-10-30 17:21:43 +0530

    browsecomponnets

--- assignments/week-11/amazonCart/src/components/BrowseProducts.jsx
+++ assignments/week-11/amazonCart/src/components/BrowseProducts.jsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './BrowseProducts.css';
 import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css'; // Don't forget to import this
+import 'react-loading-skeleton/dist/skeleton.css'; 
+import ProductCard from './ProductCard';
 
-const BrowseProducts = ({  setItemsInCart }) => {
+const BrowseProducts = ({  setItemsInCart, itemsInCart }) => {
     const [isLoading, setIsLoading] = useState(true);
     const [products, setProducts] = useState([]);
-
     useEffect(() => {
         const getProducts = async () => {
             setIsLoading(true);
@@ -24,11 +24,13 @@ const BrowseProducts = ({  setItemsInCart }) => {
         getProducts(); 
     }, []);
 
+    
+
     return (
         <div className='productListContainer'>
             <div className='productList'>
                 {isLoading ? (
-                    // Show multiple skeleton cards while loading
+                  
                     Array(5).fill(0).map((_, index) => (
                         <ProductCard key={index} isLoading={true} />
                     ))
@@ -42,35 +44,12 @@ const BrowseProducts = ({  setItemsInCart }) => {
                         />
                     ))
                 )}
+                
             </div>
         </div>
     );
 };
 
-function ProductCard({ data, setItemsInCart, isLoading }) {
-    const addToCart = () => {
-        setItemsInCart(prevItems => [...prevItems, data]); 
-    };
 
-    if (isLoading) {
-        return (
-            <div className='product'>
-                <p id='title'><Skeleton height={20} /></p> 
-                <Skeleton height={200} width="100%" /> {/* For image */}
-                <p id='price'><Skeleton width={60} /></p> 
-                <Skeleton height={35} width={100} /> {/* For button */}
-            </div>
-        );
-    }
-
-    return (
-        <div className='product'>
-            <p id='title'>{data.title}</p> 
-            <img id='thumbnail' src={data.thumbnail} alt={data.title} />
-            <p id='price'>${data.price}</p> 
-            <button id='addButton' onClick={addToCart}>Add to Cart</button> 
-        </div>
-    );
-}
 
 export default BrowseProducts;
